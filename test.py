8b893f9411bdaed7492ad29c8c84d28aae998b37
miha <mihai.husea@gmail.com>
2022-09-12 12:39:38 +0300

    update

--- test.py
+++
@@ -1,8 +0,0 @@
-def range_char(start, stop):
-    return (chr(n) for n in range(ord(start), ord(stop) + 1))
-
-for character in range_char("A", "D"):
-    print(character)
-
-lista=[str(character)+'2'for character in range_char("A", "D") ]
-print(lista)
